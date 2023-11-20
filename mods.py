eb77c36fe56a3199b8ba540ff658fccbda699b8a
multihexagon <98978765+multihexagon@users.noreply.github.com>
2023-11-19 21:46:29 -0500

    Add files via upload

---
+++ mods.py
@@ -0,0 +1,127 @@
+import sympy as sp
+import numpy as np
+from math import factorial
+
+x=sp.symbols('x')
+X = sp.symbols('x')
+
+def taylor(f,x0,n):
+    x = sp.symbols('x')
+    p = 0
+    for k in range(0,n+1):
+        df    = sp.diff(f,x,k)
+        df_x0 = df.evalf(subs={x:x0})
+        T     = (df_x0*(x-x0)**k)/factorial(k)
+        p     = p+T
+    return p
+
+def biseccion(f, a, b, tol):
+  if(f(a)*f(b)>0):
+    return '', 'La función no cumple el teorema en el intervalo'
+  else:
+    i=0
+    while(np.abs(b-a>tol)):
+      c = (a+b)/2
+      if(f(a)*f(c)<0):
+        b=c
+        i += 1
+      else:
+        a=c
+        i +=1
+
+  return c,i
+
+def p_simple(xd, yd):
+  N= len(xd)
+  M = np.zeros([N,N])
+  P = 0
+
+  for i in range(N):
+    M[i,0] = 1
+    for j in range(1, N):
+      M[i,j]=M[i,j-1]*xd[i]
+  ai=np.linalg.solve(M,yd)
+  for i in range(N):
+    P=P+ai[i]*X**i
+  return f"El polinomio interpolante es: P(X)= ',{P}"
+
+
+def lagrange(xd,yd):
+  N= len(xd)
+  P = 0
+  for i in range(N):
+    T = 1
+    for j in range(N):
+      if j != i:
+        T = T*((X-xd[j])/(xd[i]-xd[j]))
+    P=P+T*yd[i]
+  return f"El polinomio es P(x): ', {sp.expand(P)}"
+
+def mc(xd, yd):
+  m = len(xd)
+  sy = sum(yd)
+  sx = sum(xd)
+  sxy = sum(xd*yd)
+  dsx = sum(xd**2)
+  sx2 = sx**2
+  a0 = ((sy*dsx)-(sx*sxy))/((m*dsx)-sx2)
+  a1 = ((m*sxy)-(sx*sy))/((m*dsx)-sx2)
+  return a0,a1
+
+def Euler(f,a,b,h,co):
+    n = int((b-a)/h)
+    t = np.linspace(a,b,n+1)
+    yeu = [co]
+    for i in range(n):
+      yeu.append(yeu[i]+h*f(t[i],yeu[i]))
+    return t, yeu
+
+def Runge4(f,a,b,h,co):
+  n = int((b-a)/h)
+  t = np.linspace(a,b, n+1)
+  yk = [co]
+  for i in range(n):
+    k1 = h*f(t[i], yk[i])
+    k2 = h*f(t[i]+h/2, yk[i]+1/2*k1)
+    k3 = h*f(t[i]+h/2, yk[i]+1/2*k2)
+    k4 = h*f(t[i+1], yk[i]+k3)
+    yk.append(yk[i]+1/6*(k1+2*k2+2*k3+k4))
+  return t, yk
+
+def Trapecio(f,a,b,n):
+  h = (b-a)/n
+  S = 0
+  for i in range(1,n):
+    S+=f(a+i*h)
+  I =  h/2*(f(a)+2*S+f(b))
+  return I
+
+def sims13(f,a,b,n):
+    if(n%2 == 0):
+      h = (b-a)/n
+      Si = 0
+      Sp = 0
+      for i in range(1,n):
+        if(i%2==0):
+          Sp += f(a+i*h) #PARES
+        else:
+          Si += f(a+i*h) #IMPARES
+      I=(h/3)*(f(a)+2*Sp+4*Si+f(b))
+      return I
+    else:
+      print('Esta regla requiere un numero par de n')
+
+def sims38(f,a,b,n):
+    if(n%3 == 0):
+      h = (b-a)/n
+      Si = 0
+      Sp = 0
+      for i in range(1,n):
+        if(i%3==0):
+          Sp += f(a+i*h) #MULTIPLOS DE 3
+        else:
+          Si += f(a+i*h) #NO MULTIPLOS DE 3
+      I=((3*h)/8)*(f(a)+2*Sp+3*Si+f(b))
+      return I
+    else:
+      print('Esta regla requiere un numero par de n')
